468ef68ebd82562948af90a0c090e21844a91681
Codemonkey51 <contact@codemonkey51.dev>
2020-05-26 19:11:04 -0700

    Update keep_alive.py

--- pythoniaBot/keep_alive.py
+++ pythoniaBot/keep_alive.py
@@ -1,15 +1,20 @@
 from flask import Flask
 from threading import Thread
 
+
 app = Flask('')
 
 @app.route('/')
 def main():
     return "bot is online"
 
-def run():
+def run(app,none):
     app.run(host='0.0.0.0',port='8080',debug=False)
 
-def keep_alive():
-    server = Thread(target=run)
-    server.start()
+def keep_alive(keepApp = True,newApp = app):
+    if(keepApp==True):
+      server = Thread(target=run,args=(app,0))
+      server.start()
+    else:
+      server = Thread(target=run,args=(newApp,0))
+      server.start()
